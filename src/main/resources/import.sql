d7d3d23f2c346d6b416607efb0c2394ddf29c353
jmyang <qqs2615@gmail.com>
2024-06-27 01:54:12 +0900

    초기 데이터 생성

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+insert into student(user_id, user_name) values (1, '홍길동');
+insert into student(user_id, user_name) values (2, '임꺽정');
+insert into student(user_id, user_name) values (3, '김유신');
+
+insert into lecture(lecture_no, lecture_name) values (1, '수학');
+insert into lecture(lecture_no, lecture_name) values (2, '영어');
+insert into lecture(lecture_no, lecture_name) values (3, '국어');
+insert into lecture(lecture_no, lecture_name) values (4, '한국사');
+insert into lecture(lecture_no, lecture_name) values (5, '과학');
+insert into lecture(lecture_no, lecture_name) values (6, '일본어');
