0053a6a42da3aff7cf59f2a7ae4d060aa1981e9a
jmyang <qqs2615@gmail.com>
2024-06-27 01:54:43 +0900

    Repositroy 보완

--- src/main/java/com/clean/architecture/repository/HistoryRepository.java
+++ src/main/java/com/clean/architecture/repository/HistoryRepository.java
@@ -1,11 +1,19 @@
 package com.clean.architecture.repository;
 
-import com.clean.architecture.domain.dto.HistoryDto;
 import com.clean.architecture.domain.entity.History;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import javax.lang.model.element.Name;
 import java.util.List;
 
 public interface HistoryRepository extends JpaRepository<History, Integer> {
-    List<History> findAllMyHistoryByUserId(HistoryDto historyDto);
+    @Query(value = "SELECT history_no" +
+            "            , user_id" +
+            "            , schdule_no" +
+            "            , create_date" +
+            "         FROM history" +
+            "        WHERE user_id = :user_id", nativeQuery = true)
+    List<History> findAllMyHistoryByUserId(@Param("user_id") Long userId);
 }
